39be66c4090d57d35b44d684fde5f6bffe9bea8a
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-17 19:30:36 -0400

    built out server.js to use sessions

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const session = require('express-session');
+const Store = require('connect-session-knex')(session);
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -20,6 +22,25 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session({
+  name: 'chocolatechip',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: false,
+  },
+  rolling: true,
+  resave: false,
+  saveUninitialized: false,
+  store: new Store({
+    knex: require('../data/db-config'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+
+  })
+}))
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
