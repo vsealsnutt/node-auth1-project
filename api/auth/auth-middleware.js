349e842c824da2db54744b8a5c4e7565b08d14e6
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-17 19:44:46 -0400

    wrote middleware functions

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -6,8 +6,12 @@
     "message": "You shall not pass!"
   }
 */
-function restricted() {
-
+function restricted(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    next({ status: 401, message: 'You shall not pass!' });
+  }
 }
 
 /*
@@ -18,8 +22,12 @@ function restricted() {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {
-
+function checkUsernameFree(req, res, next) {
+  if (req.body.username) {
+    next({ status: 422, message: 'Username taken' });
+  } else {
+    next();
+  }
 }
 
 /*
@@ -30,8 +38,14 @@ function checkUsernameFree() {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {
-
+function checkUsernameExists(req, res, next) {
+  if (!req.body.username) {
+    res.status(401).jason({
+      message: 'Invalid credentials!'
+    });
+  } else {
+    next();
+  }
 }
 
 /*
@@ -42,8 +56,20 @@ function checkUsernameExists() {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {
-
+function checkPasswordLength(req, res, next) {
+  if (!req.body.password || req.body.password.length < 3) {
+    res.status(422).json({
+      message: 'Password must be longer than 3 chars'
+    });
+  } else {
+    next();
+  }
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+module.exports = {
+  restricted,
+  checkUsernameFree,
+  checkUsernameExists,
+  checkPasswordLength
+}
