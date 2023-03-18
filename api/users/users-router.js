758acd1ad07dcf0e1394249e518db71f7f9f0364
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-18 09:47:08 -0400

    wrote the endpoint to get users

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,7 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
-
+const router = require('express').Router();
+const { restricted } = require('../auth/auth-middleware');
+const Users = require('./users-model');
 
 /**
   [GET] /api/users
@@ -24,5 +26,14 @@
   }
  */
 
+  router.get('/', restricted, (req, res, next) => {
+    Users.find()
+      .then(users => {
+        res.status(200).json(users);
+      })
+      .catch(next);
+  })
+
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router;
